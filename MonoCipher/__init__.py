c97c78b3d3aa4ea699a2071f01bb43cb1d5bfe20
Rakesh Kanna <rakeshkanna0108@gmail.com>
2024-03-31 17:47:05 +0530

    Update __init__.py

--- MonoCipher/__init__.py
+++ MonoCipher/__init__.py
@@ -1,9 +1,17 @@
 from .SimpleEncryption import shift_encrypt, shift_decrypt
 from .ByteEncryption import byte_encrypt, byte_decrypt
 from .SaltEncryption import salt_encrypt, salt_decrypt
+from .HmacEncryption import hmac_encrypt, hmac_decrypt
+from .NonceEncryption import nonce_encrypt, nonce_decrypt
+from .MacEncryption import mac_encrypt, mac_decrypt
+from .cli import mc_cli
 
 __all__ = [
     "shift_encrypt", "shift_decrypt",
     "byte_encrypt", "byte_decrypt",
-    "salt_encrypt", "salt_decrypt"
+    "salt_encrypt", "salt_decrypt",
+    "hmac_encrypt", "hmac_decrypt",
+    "nonce_encrypt", "nonce_decrypt",
+    "mac_encrypt", "mac_decrypt",
+    "mc_cli"
 ]
