e44e0e3403e9d262ee148c630956e6f75eeffbbd
rakeshkanna-rk <rakeshkanna0108@gmail.com>
2024-07-07 17:13:27 +0530

    UPDATED cli2.py

--- MonoCipher/cli2.py
+++ MonoCipher/cli2.py
@@ -14,7 +14,7 @@ from MonoCipher.SimpleEncryption import shift_encrypt, shift_decrypt # ..checked
 VERSION = 'v0.1.4 beta'
 
 # Varibles
-breaker = '\n  ---------------——'
+breaker = '\n  —————————————————'
 
 # Header
 header = f'''{BLUE}
@@ -61,39 +61,86 @@ def checker(holder, check, err, typ: type):
                 print(err)
         
         return num
-    
 
-def shift_enc():
+def msg_():
     msg = f"{BLUE}Enter your message: {RESET}"
     msg = checker(msg, '', 'Please enter a message', str)
+    return msg
+
+
+def salt_():
+    salt = f"{BLUE}Enter your Salt: {RESET}"
+    salt = checker(salt, '', 'Please enter the Salt', str)
+    return salt
+
+def iv_():
+    iv = f"{BLUE}Enter your IV: {RESET}"
+    iv = checker(iv, '', 'Please enter the IV', str)
+    return iv  
+
+def ciphertext_():
+    ciphertext = f"{BLUE}Enter your Ciphertext: {RESET}"
+    ciphertext = checker(ciphertext, '', 'Please enter the Ciphertext', str)
+    return ciphertext
+
+def password_():
+    pws = f"{BLUE}Enter the password: {RESET}"
+    pws = checker(pws, '', 'Please enter the password', str)
+    return pws
+
+def password_fst():
+    pws = f"{BLUE}Enter your password: {RESET}"
+    pws = checker(pws, '', 'Please enter a password', str)
+    return pws
+
+def shift_enc():
+    msg = msg_()
     sht = f"{BLUE}Enter your shift value: {RESET}"
     sht = checker(sht, 0, 'Please enter a shift value (1-500)', int)
     print(f"Encrypted Message: {shift_encrypt(msg, sht)}")
     
 def shift_dec():
-    msg = f"{BLUE}Enter encrypted message: {RESET}"
-    msg = checker(msg, '', 'Please enter encrypted message', str)
+    msg = msg_()
     sht = f"{BLUE}Enter the shift value: {RESET}"
     sht = checker(sht, 0, 'Please enter the shift value (1-500)', int)
     print(f"Decrypted Message: {shift_decrypt(msg, sht)}")
 
+def byte_enc():
+    msg = msg_()
+    password = password_fst()
+    key_length = 16
+    if len(password) > key_length:
+        password = password[:key_length]
+    elif len(password) < key_length:
+        password = password.ljust(key_length, '0')
+
+    iv, ciphertext = byte_encrypt(msg, password)
+    print(f"IV: {iv} \nCiphertext: {ciphertext}")
+
+def byte_dec():
+    iv = iv_()
+    ciphertext = ciphertext_()
+    password = password_()
+
+    key_length = 16
+    if len(password) > key_length:
+            password = password[:key_length]  
+    elif len(password) < key_length:
+        password = password.ljust(key_length, '0')  
+
+    print(f"Decrypted Message: {byte_decrypt(iv, ciphertext, password)}")
+
 def salt_enc():
-    msg = f"{BLUE}Enter your message: {RESET}"
-    msg = checker(msg, '', 'Please enter a message', str)
-    pws = f"{BLUE}Enter your password: {RESET}"
-    pws = checker(pws, '', 'Please enter a password', str)
+    msg = msg_()
+    pws = password_fst()
     salt, iv, ciphertext = salt_encrypt(msg, pws)
     print(f"Salt: {salt} \nIV: {iv} \nCiphertext: {ciphertext}")
     
 def salt_dec():
-    salt = f"{BLUE}Enter your Salt: {RESET}"
-    salt = checker(salt, '', 'Please enter the Salt', str)
-    iv = f"{BLUE}Enter your IV: {RESET}"
-    iv = checker(iv, '', 'Please enter the IV', str)
-    ciphertext = f"{BLUE}Enter your Ciphertext: {RESET}"
-    ciphertext = checker(ciphertext, '', 'Please enter the Ciphertext', str)
-    pws = f"{BLUE}Enter the password: {RESET}"
-    pws = checker(pws, '', 'Please enter the password', str)
+    salt = salt_()
+    iv = iv_()
+    ciphertext = ciphertext_()
+    pws = password_()
     try:
         print(f"Decrypted Message: {salt_decrypt(salt, iv, ciphertext, pws)}")
     except Exception:
@@ -104,7 +151,8 @@ def salt_dec():
 def start():
     options(option=[('Shift Encryption', lambda: shift_enc()),
                     (f'Shift Decryption {breaker}', lambda: shift_dec()),
-                    # TODO: Add Byte Encryption and Decryption
+                    ('Byte Encryption', lambda: byte_enc()),
+                    (f'Byte Decryption {breaker}', lambda: byte_dec()),
                     ('Salt Encryption', lambda: salt_enc()),
                     (f'Salt Decryption {breaker}', lambda: salt_dec())],
                     # TODO: Add other methods
